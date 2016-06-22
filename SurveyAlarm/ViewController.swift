3ef8d709b340940e52c1c0e8b3e0dd3da3877557
Cody <codyli332@gmail.com>
2016-06-22 10:23:03 -0400

    App asks for Notification permissions

---
+++ SurveyAlarm/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SurveyAlarm
+//
+//  Created by Cody Li on 6/21/16.
+//  Copyright © 2016 Cody Li. All rights reserved.
+//
+
+import UIKit
+import ResearchKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+//        let myStep = ORKInstructionStep(identifier: "intro")
+//        myStep.title = "Welcome to ResearchKit"
+        setNotifications()
+    }
+    
+    //Notifications
+    func setNotifications(){
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+    }
+
+}
+
