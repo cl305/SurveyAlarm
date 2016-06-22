0ebb16874dae0bdf8091dd3df764bf032260c95d
Cody <codyli332@gmail.com>
2016-06-22 16:07:08 -0400

    Fixed unrecognized selector error with date picker...removed deprecated event

--- SurveyAlarm/ViewController.swift
+++ SurveyAlarm/ViewController.swift
@@ -11,6 +11,8 @@ import ResearchKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timePicker: UIDatePicker!
+    var strTime : NSDate?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,13 +26,40 @@ class ViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
 //        let myStep = ORKInstructionStep(identifier: "intro")
 //        myStep.title = "Welcome to ResearchKit"
-        setNotifications()
+        setNotificationPermission()
+        
     }
     
     //Notifications
-    func setNotifications(){
+    func setNotificationPermission(){
         let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
         UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+    }
+    @IBOutlet weak var notificationButton: UIButton!
+    
+    @IBAction func setAlarms(sender: AnyObject) {
+        print("hey")
+        var timeFormatter = NSDateFormatter()
+                    timeFormatter.dateFormat = "hh:m a"
+//        timeFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+        var strDate = timeFormatter.stringFromDate(strTime!)
+        print(strDate)
+    }
+    
+    func setNotification(){
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 5)
+        notification.alertBody = "Hey you!"
+        notification.alertAction = "Ok"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.userInfo = ["CustomField1": "w00t"]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+
+    }
+
+    @IBAction func setTime(sender: AnyObject) {
+        print("heyyyyyy")
+        strTime = timePicker.date
     }
 
 }
